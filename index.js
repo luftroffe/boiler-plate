e0d67e8fe7e21c323c2b4e86906a89c05331b8fc
Kim Jaewoo <107554680+luftroffe@users.noreply.github.com>
2022-09-11 15:02:19 +0900

    change mongoURI invisible

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 5000
 
+const config = require('./config/key')
+
+const { User } = require("./models/User")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:zvnSZWUcDdTKxDeH@boiler-plate.cochrgg.mongodb.net/?retryWrites=true&w=majority', {})
+mongoose.connect(config.mongoURI, {})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World! Hi'))
+app.get('/', (req, res) => res.send('Hello World! Bye'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
